63c3223da69a953aa6c75c1da8a59c38bb3eef4f
Rob Kodey <rkodey+github@gmail.com>
2025-07-21 23:45:51 -0400

    Add light/dark theme awareness to restoring-window

--- src/js/restoring-window.js
+++ src/js/restoring-window.js
@@ -1,9 +1,13 @@
 import  { gsUtils }               from './gsUtils.js';
+import  { gsStorage }             from './gsStorage.js';
 
 (() => {
   'use strict';
 
-  gsUtils.documentReadyAndLocalisedAsPromised(document).then(function() {
-    //do nothing
+  gsUtils.documentReadyAndLocalisedAsPromised(document).then(async () => {
+
+    //Set theme
+    document.body.classList.add(await gsStorage.getOption(gsStorage.THEME) === 'dark' ? 'dark' : null);
+
   });
 })();
